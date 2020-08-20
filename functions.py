d1b429711e948c54e5b9d516c76782c442499f35
Dawit <dawit@ginetta.net>
2020-08-20 07:32:58 +0200

    Access functions from an imported module

--- functions.py
+++ functions.py
@@ -1,8 +1,9 @@
+import functiontobeimported
 def multiplication(x, y, z):
     return x*y*z
 
-def square(x):
-    return x*x
+# def square(x):
+#     return x*x
 
 for i in range(10):
-    print(f"The square of {i} is {square(i)}")
+    print(f"The square of {i} is {functiontobeimported.square(i)}")
