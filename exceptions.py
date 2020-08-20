12e57282d7d54bdba6058203fbed619994935a62
Dawit <dawit@ginetta.net>
2020-08-20 09:30:04 +0200

    Introduce an error handler

--- exceptions.py
+++ exceptions.py
@@ -1,6 +1,13 @@
+import sys
+
 x = int(input("x: "))
 y = int(input("y: "))
 
+try:
+    result = x/y
+except ZeroDivisionError:
+    # introducing handling errors
+    print("Error: Cannot divide by 0.")
 result = x/y
 
 print(f"{x} / {y} = {result}")
